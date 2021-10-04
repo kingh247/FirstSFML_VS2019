0ded379cb376d52b546118cb9961962456b042e8
Darryl <dk.charles@ulster.ac.uk>
2021-10-04 10:53:56 +0100

    Comments added

--- FirstSFML/main.cpp
+++ FirstSFML/main.cpp
@@ -1,23 +1,27 @@
+/* 
+* A basic SFML application to build upon - Darryl Charles
+* SFML Documentation https://www.sfml-dev.org/documentation/2.5.1/ 
+*/
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");    // render a 200x200 Window
+    sf::CircleShape shape(100.f);                                       // create a circle object called shape
+    shape.setFillColor(sf::Color::Green);                               // colour shape Green
 
-    while (window.isOpen())
+    while (window.isOpen())                                             // This is the Windows application loop - infinite loop until closed
     {
-        sf::Event event;
+        sf::Event event;                                                // Windows is event driven
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        window.clear();                                                 // Clear render buffer
+        window.draw(shape);                                             // Draw the shape to the buffer (in this case at default position)
+        window.display();                                               // Display the graphics from the buffer to the display
     }
 
     return 0;
